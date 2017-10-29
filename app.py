c201ff7d592a904dcd9fb13fc19acde5600b8138
TheaZh <qz2271@columbia.edu>
2017-10-29 17:55:45 -0400

    modify extract tuples method

--- app.py
+++ app.py
@@ -57,48 +57,80 @@ def get_plain_text(url):
     # return as an array format
     return [text.encode('utf-8')]
 
+# get a whole sentence
+def from_words_to_sentence(sentence):
+    newsentence = ""
+    for x in sentence.tokens:
+        newsentence += " " + x.word
+    return newsentence
 # get sentences from plain text
 def get_sentences(plain_txt):
     doc = client.annotate(text=plain_txt, properties=properties_pipeline1)
     sentences = []
     for sentence in doc.sentences:
-        newsentence = ""
-        for x in sentence.tokens:
-            newsentence += " " + x.word
+        newsentence = from_words_to_sentence(sentence)
         # newsentence = newsentence.encode('utf8','replace')
         sentences.append(newsentence.encode('utf-8'))
     # print 'sentences: ', sentences
     return sentences
 
 # analyze sentences to extract tuples
-def extract_tuples(sentences, relation_group, threshold):
+def extract_tuples(query_sentences, relation_group, threshold):
+    num_of_relations = 0  # the overall number of relations extracted from this website
+    num_of_valid_relations = 0 # the number of valid relations (including relations whose confidence below threshold)
     tuples = []
-    doc = client.annotate(text=sentences, properties=properties_pipeline2)
-    for index in range(0, len(sentences)):
-        # print index, " --- ", doc.sentences[index].relations
-        if len(doc.sentences[index].relations) is 0:
+    doc = client.annotate(text=query_sentences, properties=properties_pipeline2)
+    sentences = doc.sentences
+    for sentence in sentences:
+        print " --- ", from_words_to_sentence(sentence)
+        relations = sentence.relations
+        if len(relations) is 0:
             continue
-        if not doc.sentences[index].relations[0]:
-            print 'len 0'
-            continue
-        # if doc.sentences[index].relations[0].probabilities.keys()[0] is not relation_group:
-        #     continue
-        word1 = doc.sentences[0].entities[0].value
-        word2 = doc.sentences[0].entities[1].value
-        confidence = float(doc.sentences[index].relations[0].probabilities[relation_group])
-        # print '/t', confidence
-        # if the most confident group is relation_group
-        if doc.sentences[index].relations[0].probabilities.key()[0] is relation_group:
-            print '== == == == == == == = EXTRACTED RELATION == == == == == == == ='
-            print 'Sentence: ' , doc.sentences[index]
-            print 'Relation Type: ', relation_group,
-            tuple = []
-            tuple.append(word1)
-            tuple.append(word2)
-            tuple.append(confidence)
-            tuples.append(tuple)
+        for relation in relations:
+            if not relation:
+                # print 'len 0'
+                continue
+            num_of_relations += 1 # count relations
+            # print "Relation:::::::", relation, "\ntype:::::", relation.probabilities.keys()[0]
+            probability_dic = sorted(relation.probabilities.items(), key=lambda (k, v) : -float(v))
+            if probability_dic[0][0] == relation_group:
+                num_of_valid_relations += 1 # count valid relations
+                word1 = relation.entities[0].value
+                type1 = relation.entities[0].type
+                word2 = relation.entities[1].value
+                type2 = relation.entities[1].type
+                confidence = float(relation.probabilities[relation_group])
+
+                plain_sentence = from_words_to_sentence(sentence)
+
+                print '============== EXTRACTED RELATION =============='
+                print 'Sentence:' , plain_sentence
+                print 'Relation Type:', relation_group, ' | ' \
+                      'Confidence=', confidence, ' | ' \
+                      'EntityType1=', type1, ' | ' \
+                      'EntityValue1=', word1, ' | ' \
+                      'EntityType2=', type2, ' | ' \
+                      'EntityValue2=', word2
+                print '============== END OF RELATION DESC =============='
+
+                # save tuples whose confidence above threshold
+                if confidence >= threshold:
+                    tuple = []
+                    tuple.append(word1+' ('+type1+')')
+                    tuple.append(word2+' ('+type2+')')
+                    tuple.append(round(confidence,3))
+                    tuples.append(tuple)
             # print tuple
+    print 'Relations extracted from this website: ' , num_of_valid_relations , ' (Overall: ' , num_of_relations , ')'
     return tuples
+
+
+def relation_print_format(result_tuples, relation_type):
+    print 'Pruning relations below threshold...\n' \
+          'Number of tuples after pruning: ' , len(result_tuples) , '\n' \
+          '================== ALL RELATIONS ================='
+    for tuple in result_tuples:
+        print "Relation Type: {}| Confidence: {}| Entity #1: {}|Entity #2: {}".format(relation_type.ljust(20),str(tuple[2]).ljust(10), tuple[0].ljust(37), tuple[1])
 
 
 
@@ -158,7 +190,7 @@ if __name__ == '__main__':
 
     relation_id = 4
     query = "bill gates microsoft".encode('utf-8')
-    threshold = 0.35
+    threshold = 0.22
 
 
     relation_group = groups[relation_id-1]
@@ -170,12 +202,12 @@ if __name__ == '__main__':
         #    Print Format - Parameters
         ##################################
     print 'Parameters:\n' \
-          'Client key        = ', GOOGLE_API, ''\
-          '\nEngine key      = ', GOOGLE_ENGINE_ID, ''\
-          '\nRelation        = ', relation_group, ''\
-          '\nThreshold       = ', threshold, '' \
-          '\nQuery           = ', query, ''\
-          '\n# of Tuples     = ', number_of_tuples
+          'Client key      = ', GOOGLE_API, '\n'\
+          'Engine key      = ', GOOGLE_ENGINE_ID, '\n'\
+          'Relation        = ', relation_group, '\n'\
+          'Threshold       = ', threshold, '\n' \
+          'Query           = ', query, '\n'\
+          '# of Tuples     = ', number_of_tuples
 
         ###################################
         #    End of Printing Parameters
@@ -184,7 +216,9 @@ if __name__ == '__main__':
     # get 10 urls
     # URLs = search_google(GOOGLE_API, GOOGLE_ENGINE_ID, query)
 
-    URLs = ['https://news.microsoft.com/exec/bill-gates/', 'https://en.wikipedia.org/wiki/Bill_Gates', 'https://www.theverge.com/2017/8/15/16148370/bill-gates-microsoft-shares-sale-2017', 'https://www.biography.com/people/bill-gates-9307520', 'http://www.telegraph.co.uk/technology/0/bill-gates/', 'https://www.cnbc.com/2017/09/25/bill-gates-microsoft-ceo-satya-nadella-talk-about-leadership.html', 'http://www.zdnet.com/article/bill-gates-stake-in-microsoft-is-now-just-1-3-percent/', 'https://www.wsj.com/articles/a-rare-joint-interview-with-microsoft-ceo-satya-nadella-and-bill-gates-1506358852', 'https://twitter.com/billgates', 'https://www.youtube.com/watch?v=rOqMawDj0LQ']
+    URLs = ['https://en.wikipedia.org/wiki/Bill_Gates']
+            # 'https://news.microsoft.com/exec/bill-gates/',
+            #'https://www.theverge.com/2017/8/15/16148370/bill-gates-microsoft-shares-sale-2017', 'https://www.biography.com/people/bill-gates-9307520', 'http://www.telegraph.co.uk/technology/0/bill-gates/', 'https://www.cnbc.com/2017/09/25/bill-gates-microsoft-ceo-satya-nadella-talk-about-leadership.html', 'http://www.zdnet.com/article/bill-gates-stake-in-microsoft-is-now-just-1-3-percent/', 'https://www.wsj.com/articles/a-rare-joint-interview-with-microsoft-ceo-satya-nadella-and-bill-gates-1506358852', 'https://twitter.com/billgates', 'https://www.youtube.com/watch?v=rOqMawDj0LQ']
 
 
     print "URLs: ", URLs
@@ -193,17 +227,24 @@ if __name__ == '__main__':
     result_tuples = []
 
     for url in URLs:
-        plain_text = get_plain_text(url)
+        print 'Processing: ' + url
+        # plain_text = get_plain_text(url)
+        plain_text = ['William Henry Gates III (born October 28, 1955) is an American business magnate , investor , author , philanthropist , and co-founder of the Microsoft Corporation along with Paul Allen.']
         sentences = get_sentences(plain_text)
+        print sentences
         tuples = extract_tuples(sentences, relation_group, threshold)
         if len(tuples) <1:
             continue
         else:
             result_tuples.extend(tuples)
+
+    # sorted by confidence (descending)
     result_tuples = sorted(result_tuples, key=lambda x: -float(x[2]))
-    print "tuples:  " ,result_tuples
-    for res in result_tuples:
-        print res[0], '--', res[1], '--', res[2]
+
+    relation_print_format(result_tuples, relation_group)
+    # print "tuples:  " ,result_tuples
+    # for res in result_tuples:
+    #     print res[0], '--', res[1], '--', res[2]
 
     #############################################
     #             End of Debug
